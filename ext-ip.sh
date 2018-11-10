666c54d0c14e871ec9654ee3bf7280e2f2a58806
salida <salidasbp@hotmail.com>
2018-11-10 13:31:10 +0200

    Simple script to find if your ddns ip is the same with your external IP, usefull troubleshooting tool

---
+++ ext-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BLUE='\e[44m'
+GREEN='\033[32;1m'
+#RED='\e[41m'
+NC='\e[0m'
+#ENTER you domain in the line below
+#example dm=google.gr
+dm=
+dip=$(host $dm |awk {'print $4'})
+ex=$(curl -s ipinfo.io/ip)
+echo -e " ┌──────────────────────────────────────────────────────────────┐"
+echo -e " │  $GREEN Domain$NC        : $BLUE$dm$NC     	       		│"
+echo -e " │  $GREEN EXT IP$NC        : $BLUE$ex$NC				│"
+echo -e " │  $GREEN NO  IP$NC        : $RED$dip$NC				│"
+echo " └──────────────────────────────────────────────────────────────┘"
