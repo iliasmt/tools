7f154a76c727a50f5493220cab58a351dc65b29b
iliasmt <salidasbp@hotmail.com>
2021-05-22 20:19:38 +0300

    remove comment

--- get-winpc-updates.bat
+++ get-winpc-updates.bat
@@ -7,6 +7,6 @@ REM disabled commands just in case
 REM systeminfo |findstr %KBNUM%
 REM Get-HotFix | Sort-Object InstalledOn -Descending |findstr %KBNUM%
 
-REM wmic qfe list |findstr %KBNUM%
+wmic qfe list |findstr %KBNUM%
 
 pause
